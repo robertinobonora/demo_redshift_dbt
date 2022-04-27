cbf23e0e402e548bc7e69d759236b1429bfd3515
Robertino Bonora <robertinobonora@MacBook-Pro-de-Robertino.local>
2022-04-27 17:43:30 -0400

    Nuevo commit del proyecto dbt_redshift_demo

--- macros/dbt_ejecucion_modelo.sql
+++ macros/dbt_ejecucion_modelo.sql
@@ -1,6 +1,6 @@
 {% macro dbt_ejecucion_modelo(this,nombre_ejecucion,nombre_metrica) -%}
 {% if nombre_metrica == 'cantidad_registros' %}
 insert into dev.dbt_staging.dbt_metrica_ejecucion (nombre_modelo,nombre_ejecucion,nombre_metrica,val_metrica) 
-select '{{ this }}','{{ nombre_ejecucion }}','{{ nombre_metrica }}',count(1) from {{ this }}; commit;
+select '{{ this.name }}','{{ nombre_ejecucion }}','{{ nombre_metrica }}',count(1) from {{ this }}; commit;
 {% endif %}
 {%- endmacro %}
